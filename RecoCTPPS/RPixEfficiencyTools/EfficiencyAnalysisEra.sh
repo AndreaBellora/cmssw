471a266c75c98bedc7fb7a5da23bd5979b6c0b0b
AndreaBellora <andrea.bellora@cern.ch>
2020-10-01 14:14:26 +0200

    Fixed JSON files

--- RecoCTPPS/RPixEfficiencyTools/EfficiencyAnalysisEra.sh
+++ RecoCTPPS/RPixEfficiencyTools/EfficiencyAnalysisEra.sh
@@ -6,6 +6,7 @@ else
 	export CMSSW_BASE=${2}
 	export SCRAM_ARCH=slc7_amd64_gcc700
 	export X509_USER_PROXY=$CMSSW_BASE/src/RecoCTPPS/RPixEfficiencyTools/x509up_u93252
+	addJSON_A="useJsonFile=True jsonFileName=/eos/project-c/ctpps/Operations/DataExternalConditions/2018/CMSgolden_2RPGood_anyarms_EraA.json"
 	if true 
 	then	
 		addJSON="useJsonFile=True jsonFileName=/eos/project-c/ctpps/Operations/DataExternalConditions/2018/CMSgolden_2RPGood_anyarms_Era${1}.json"
@@ -16,12 +17,12 @@ else
 	cd $CMSSW_BASE/src/RecoCTPPS/RPixEfficiencyTools/
 	eval `scramv1 runtime -sh`
 	set -x
-	eval "cmsRun python/EfficiencyAnalysis_cfg.py sourceFileList=InputFiles/Era${1}${fileSuffix}.dat outputFileName=OutputFiles/Era${1}${fileSuffix}.root bunchSelection=NoSelection useJsonFile=True jsonFileName=/eos/project/c/ctpps/Operations/DataExternalConditions/2018/CMSgolden_2RPGood_anyarms_Era${1}.json"
-	eval "cmsRun python/RefinedEfficiencyAnalysis_cfg.py sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}.root outputFileName=OutputFiles/Era${1}_refinedEfficiency${fileSuffix}.root ${addJSON}"
+	eval "cmsRun python/EfficiencyAnalysis_cfg.py sourceFileList=InputFiles/Era${1}${fileSuffix}.dat outputFileName=OutputFiles/Era${1}${fileSuffix}.root bunchSelection=NoSelection ${addJSON}"
+	eval "cmsRun python/RefinedEfficiencyAnalysis_cfg.py sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}.root outputFileName=OutputFiles/Era${1}_refinedEfficiency${fileSuffix}.root ${addJSON_A}"
 	eval "cmsRun python/InterpotEfficiency_cfg.py sourceFileList=InputFiles/Era${1}${fileSuffix}.dat outputFileName=OutputFiles/Era${1}_interpotEfficiency${fileSuffix}.root ${addJSON}"
-	eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_refinedEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi${fileSuffix}.root"
-	eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_refinedEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi${fileSuffix}_useMultiRPProtons.root useMultiRPProtons=True"
-	# eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_interpotEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi_fromInterpot${fileSuffix}.root useInterPotEfficiency=True"
-	eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_interpotEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi_fromMultiRP${fileSuffix}.root useMultiRPEfficiency=True"
+	eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_refinedEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi${fileSuffix}.root ${addJSON_A}"
+	eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_refinedEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi${fileSuffix}_useMultiRPProtons.root useMultiRPProtons=True ${addJSON_A}"
+	# eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_interpotEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi_fromInterpot${fileSuffix}.root useInterPotEfficiency=True ${addJSON_A}"
+	eval "cmsRun python/EfficiencyVsXi_cfg.py era=${1} sourceFileList=InputFiles/EraA${fileSuffix}.dat efficiencyFileName=OutputFiles/Era${1}_interpotEfficiency${fileSuffix}.root outputFileName=OutputFiles/Era${1}_efficiencyVsXi_fromMultiRP${fileSuffix}.root useMultiRPEfficiency=True ${addJSON_A}"
 	set +x
 fi
